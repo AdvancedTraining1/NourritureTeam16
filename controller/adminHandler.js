57b238f5a3db749a42fc7035b7cc1d82aa84be1b
root <root@hh-Latitude-E5400.(none)>
2014-11-15 21:02:31 +0800

    update

--- controller/adminHandler.js
+++ controller/adminHandler.js
@@ -16,28 +16,31 @@ AdminHandler.addUser = function(req, res)
 {
     req.on('data',function(data)
     {
-        var obj = JSON.parse(data.toString());
-        var str = '信息为:' + obj.name + obj.tel + obj.account + obj.type;
+        //var obj = JSON.parse(data.toString());
+        //var str = '信息为:' + obj.name + obj.tel + obj.account + obj.type;
 
-        var user = createUser();
-        UserDao.save(user,function (err, data)
+        for(var i = 0; i < 10; ++i)
         {
-            if(err)
+            var user = createUser(i);
+            UserDao.save(user,function (err, data)
             {
-                console.log(err);
+                if(err)
+                {
+                    console.log(err);
 
-            }else
-            {
+                }else
+                {
 
-                console.log(data);
+                    console.log(data);
 
-            }
+                }
 
-        });
-        res.send(str);
+            });
+        }
 
 
-
+        //res.send("admin/add");
+        res.json(200, {message: "admin/add"});
     });
 };
 
@@ -45,11 +48,11 @@ AdminHandler.updateUser = function(req, res)
 {
     req.on('data',function(data)
     {
-        var obj = JSON.parse(data.toString());
-        var str = '信息为:' + obj.name + obj.tel + obj.account + obj.type;
+        //var obj = JSON.parse(data.toString());
+        //var str = '信息为:' + obj.name + obj.tel + obj.account + obj.type;
 
-        var conditions = {account : "12221"};
-        var update = {$set : {phone : "12345",sex : "0"}};
+        var conditions = {account : "3"};
+        var update = {$set : {phone : "3",sex : "0"}};
         var options = {update : true};
         UserDao.update(conditions,update,options,function (err, data)
         {
@@ -65,8 +68,8 @@ AdminHandler.updateUser = function(req, res)
             }
 
         });
-        res.send(str);
-
+        //res.send("admin/update");
+        res.json(200, {message: "admin/update"});
 
     });
 };
@@ -86,18 +89,18 @@ AdminHandler.getAllUsers = function(req,res)
 
         }
 
-
-
     });
+    //res.send("admin/getAllUsers");
+    res.json(200, {message: "admin/getAllUsers"});
 }
 
 AdminHandler.getUserById = function(req,res)
 {
     req.on('data',function(data)
     {
-        var obj = JSON.parse(data.toString());
-        var id = obj.userid;
-        UserDao.getUserById(12221,function (err, data)
+        //var obj = JSON.parse(data.toString());
+        //var id = obj.userid;
+        UserDao.getUserById("5",function (err, data)
         {
             if(err)
             {
@@ -110,19 +113,21 @@ AdminHandler.getUserById = function(req,res)
             }
 
         });
-        res.send(obj.userid);
-
+        //res.send("admin/getUserById");
+        res.json(200, {message: "admin/getUserById"});
     });
 }
 
 
 AdminHandler.deleteUser = function(req,res)
 {
+//console.log("234324");
     req.on('data',function(data)
     {
         var id =[];
-        var obj = JSON.parse(data.toString());
-        id[0] = obj.userid;
+        //var obj = JSON.parse(data.toString());
+        id[0] = "6";
+        id[1] = "7";
         UserDao.delete(id,function (err, data)
         {
             if(err)
@@ -136,31 +141,32 @@ AdminHandler.deleteUser = function(req,res)
             }
 
         });
-        res.send(obj.userid);
-
+        //res.send("admin/delete");
+        res.json(200, {message: "admin/delete"});
     });
 
 }
 
-function createUser()
+function createUser(num)
 {
 
     var user = new userModel();
-    user.username="userName1";
-    user.account="12221";
-    user.password="123";
+    var temp = num;
+    user.username = num;
+    user.account= num;
+    user.password= num;
     user.type = 1;
-    user.phone = "12345678910111";
+    user.phone = num;
     user.sex = 1;
     user.head = "headIconPath";
-    var friends0 =[];
-    friends0[0] = {id:120,account:"amount1"};
-    friends0[1] = {id:121,account:"amount2"};
-    user.friends = friends0;
-    var fan0 =[];
-    fan0[0] = {id:120,account:"amount1"};
-    fan0[1] = {id:121,account:"amount2"};
-    user.fans = fan0;
+//    var friends0 =[];
+//    friends0[0] = {id:120,account:"amount1"};
+//    friends0[1] = {id:121,account:"amount2"};
+//    user.friends = friends0;
+//    var fan0 =[];
+//    fan0[0] = {id:120,account:"amount1"};
+//    fan0[1] = {id:121,account:"amount2"};
+//    user.fans = fan0;
     user.recipe_count = 0;
     user.topic_count = 0;
     user.blog_count = 0;
