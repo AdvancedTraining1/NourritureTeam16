76f514a6470a2f1cf24ab55435c66dba65d5236f
Chen MengMeng <mengJaeSuk@gmail.com>
2014-11-23 10:06:09 +0800

    recipe create page_2

--- routes/common.js
+++ routes/common.js
@@ -6,19 +6,23 @@
 
 module.exports = function (app) {
 
-    app.get('/top', function(req,res){
-        res.render("common/top");
-    });
+    //app.get('/top', function(req,res){
+    //    res.render("common/top");
+    //});
+    //
+    //app.get('/index', function(req,res){
+    //    res.render("common/index");
+    //});
+    //
+    //app.get('/main', function(req,res){
+    //    res.render("common/main");
+    //});
+    //
+    //app.get('/bottom', function(req,res){
+    //    res.render("common/bottom");
+    //});
 
     app.get('/index', function(req,res){
-        res.render("common/index");
-    });
-
-    app.get('/main', function(req,res){
         res.render("common/main");
-    });
-
-    app.get('/bottom', function(req,res){
-        res.render("common/bottom");
     });
 };
