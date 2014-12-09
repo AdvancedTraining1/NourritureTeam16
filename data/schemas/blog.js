5e8ffe955c79b9ec3c6d1e6e765b5da5bf04d00d
mengchi6131 <mengchi6131@gmail.com>
2014-12-09 20:42:24 +0800

    rmc

---
+++ data/schemas/blog.js
@@ -0,0 +1,32 @@
+/**
+ * Created by mengchi on 14-10-17.
+ */
+
+var mongoose = require('mongoose');
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var BlogSchema = new mongoose.Schema({
+    title: { type: String, index: true },
+    content:{ type: String},
+    type:{type:String},
+    create_at: { type: Date, default: Date.now },
+    update_at: { type: Date, default: Date.now },
+    author: {
+        id: ObjectId,
+        account: String },
+
+    praiseList:{
+        id: ObjectId,
+        account: String
+    },
+
+    comment_list: { type:ObjectId},
+
+    praise_count: { type: Number, default: 0 },
+    favorite_count:{ type: Number, default: 0 },
+    comment_count: { type: Number, default: 0 }
+
+});
+
+module.exports = BlogSchema;
