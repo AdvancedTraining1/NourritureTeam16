aff7c138337b492620f0d13d1e26d2707f4cc532
mengchi6131 <mengchi6131@gmail.com>
2014-12-13 23:41:00 +0800

    rmc

--- public/angularController/commonController.js
+++ public/angularController/commonController.js
@@ -5,6 +5,8 @@ function IndexPage($scope, $routeParams ,$http, $location){
 
     $(function(){
         queryRecipe();
+        qureyTopic();
+        qureyBlog();
     });
 
     function queryRecipe(){
@@ -14,6 +16,31 @@ function IndexPage($scope, $routeParams ,$http, $location){
             url: api + '?pageNo=1&pageSize=3'
         }).success(function(data, status) {
             $scope.recipes = data.root;
+        });
+    }
+
+
+    function qureyTopic(){
+        var api = "/topic/showTopicList";
+        $http({
+            method: 'GET',
+            url: api + '?pageNo=1&pageSize=3'
+        }).success(function(data, status) {
+            $scope.topics = data.topics;
+
+        });
+    }
+
+    function qureyBlog(){
+        var api = "/blog/showBlogList";
+        $http({
+            method: 'GET',
+            url: api + '?pageNo=1&pageSize=6'
+        }).success(function(data, status) {
+            $scope.blogs = data.root;
+
+        }).error(function(data, status) {
+
         });
     }
 }
