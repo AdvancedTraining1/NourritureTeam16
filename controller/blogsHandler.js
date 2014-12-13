c94ca3cb4f8c40d55a0b6c77d401095affd0167d
mengchi6131 <mengchi6131@gmail.com>
2014-12-13 20:55:14 +0800

    rmc

--- controller/blogsHandler.js
+++ controller/blogsHandler.js
@@ -571,12 +571,33 @@ BlogHander.checkCollction=function (req, res) {
     });
 }
 
-function getTime(){
-    var date = new Date();
-    var dateStr = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-    console.log(dateStr);
-    return dateStr;
+Date.prototype.format =function(format)
+{
+    var o = {
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    }
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+        (this.getFullYear()+"").substr(4- RegExp.$1.length));
+    for(var k in o)if(new RegExp("("+ k +")").test(format))
+        format = format.replace(RegExp.$1,
+                RegExp.$1.length==1? o[k] :
+                ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
 }
+
+function getTime(){
+
+    var data =new Date().format('yyyy-MM-dd hh:mm:ss');
+    console.log(data);
+    return data;
+}
+
 
 
 module.exports = BlogHander;
